e457ec88b41d1245cc4a961870bd6c1ad5cd719c
boissonnat <alexis.boissonnat@gmail.com>
2011-02-11 14:56:41 +0100

    remove useless comment lines

--- archetype/src/main/resources/archetype-resources/src/main/java/MyEntity.java
+++ archetype/src/main/resources/archetype-resources/src/main/java/MyEntity.java
@@ -1,7 +1,6 @@
 #set( $symbol_pound = '#' )
 #set( $symbol_dollar = '$' )
 #set( $symbol_escape = '\' )
-//package ${packageInPathFormat};
 package ${package};
 
 import org.compass.annotations.Searchable;
