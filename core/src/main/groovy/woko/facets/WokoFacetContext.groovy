1445c9ee52d17c349630b6cad831f8ccf69b6361
remi <remi@rvkb.com>
2011-02-14 10:40:20 +0100

    request in facet context

--- core/src/main/groovy/woko/facets/WokoFacetContext.groovy
+++ core/src/main/groovy/woko/facets/WokoFacetContext.groovy
@@ -4,23 +4,31 @@ import net.sourceforge.jfacets.impl.DefaultFacetContext
 import woko.Woko
 import net.sourceforge.jfacets.IProfile
 import net.sourceforge.jfacets.FacetDescriptor
+import javax.servlet.http.HttpServletRequest
 
 class WokoFacetContext extends DefaultFacetContext {
 
   private Woko woko
+  private HttpServletRequest request
 
   def WokoFacetContext(
       String facetName,
       IProfile profile,
       Object targetObject,
       FacetDescriptor facetDescriptor,
-      Woko woko) {
+      Woko woko,
+      HttpServletRequest request) {
     super(facetName, profile, targetObject, facetDescriptor)
     this.woko = woko
+    this.request = request
   }
 
   Woko getWoko() {
     return woko
+  }
+
+  HttpServletRequest getRequest() {
+    return request
   }
 
 }
--- core/src/main/groovy/woko/facets/WokoFacetContextFactory.groovy
+++ core/src/main/groovy/woko/facets/WokoFacetContextFactory.groovy
@@ -5,18 +5,41 @@ import net.sourceforge.jfacets.IFacetContext
 import net.sourceforge.jfacets.IProfile
 import net.sourceforge.jfacets.FacetDescriptor
 import woko.Woko
+import net.sourceforge.stripes.controller.Interceptor
+import net.sourceforge.stripes.controller.LifecycleStage
+import net.sourceforge.stripes.controller.Intercepts
+import net.sourceforge.stripes.controller.ExecutionContext
+import net.sourceforge.stripes.action.Resolution
+import javax.servlet.http.HttpServletRequest
 
-class WokoFacetContextFactory implements IFacetContextFactory {
+@Intercepts([LifecycleStage.RequestInit, LifecycleStage.RequestComplete])
+class WokoFacetContextFactory implements IFacetContextFactory, Interceptor {
 
   private final Woko woko
+
+  private ThreadLocal<HttpServletRequest> requests = new ThreadLocal<HttpServletRequest>()
 
   def WokoFacetContextFactory(Woko woko) {
     this.woko = woko;
   }
 
   IFacetContext create(String name, IProfile profile, Object targetObject, FacetDescriptor facetDescriptor) {
-    return new WokoFacetContext(name, profile, targetObject, facetDescriptor, woko)
+    return new WokoFacetContext(name, profile, targetObject, facetDescriptor, woko, requests.get())
   }
 
+  Resolution intercept(ExecutionContext context) throws Exception {
+    LifecycleStage stage = context.lifecycleStage
+    HttpServletRequest request = context.actionBeanContext.request
+    if (stage==LifecycleStage.RequestInit) {
+      requests.set(request)
+    } else if (stage.equals(LifecycleStage.RequestComplete)) {
+      requests.remove()
+    }
+    try {
+      return context.proceed();
+    } catch(Exception e) {
+      throw e
+    }
+  }
 
 }
