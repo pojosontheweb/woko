3c50605fd771856ddba28a2e0c55b648ffda2b5e
boissonnat <alexis.boissonnat@gmail.com>
2011-02-17 22:39:43 +0100

    Localize Navbar and add the corresponding test

---
+++ webtests/webtests-localization/src/test/groovy/woko/webtests/localization/NavBarTest.groovy
@@ -0,0 +1,37 @@
+package woko.webtests.localization
+
+import woko.webtests.WebTestBase
+
+/**
+ * Test all links provide by the different navbar (Guest and devel).<br/>
+ * We don't test here the linked actions. They are tested in other web test module.
+ */
+class NavBarTest extends WebTestBase {
+
+  void testAuthenticationWithHome() {
+    webtest("test navbar") {
+
+      // Test for guest
+      goToPage '/home'
+
+      verifyText 'home'
+      verifyXPath xpath:"/html/body/div/div[2]/div/ul/li/a[@href='/woko-webtests/home']"
+
+      // Test for devel
+      login()
+      goToPage '/home'
+
+      verifyText 'home'
+      verifyXPath xpath:"/html/body/div/div[2]/div/ul/li/a[@href='/woko-webtests/home']"
+      verifyText 'find'
+      verifyXPath xpath:"/html/body/div/div[2]/div/ul/li/a[@href='/woko-webtests/find']"
+      verifyText 'create'
+      verifyXPath xpath:"/html/body/div/div[2]/div/ul/li/a[@href='/woko-webtests/create']"
+      verifyText 'woko studio'
+      verifyXPath xpath:"/html/body/div/div[2]/div/ul/li/a[@href='/woko-webtests/studio']"
+
+
+
+    }
+  }
+}
