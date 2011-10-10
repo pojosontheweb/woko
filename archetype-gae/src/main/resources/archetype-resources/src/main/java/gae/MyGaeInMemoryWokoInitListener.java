063541292382c577a90bc91f1177458dbfabf9ce
boissonnat <alexis.boissonnat@cstb.fr>
2011-10-10 14:21:04 +0200

    Add missing imports

--- archetype-gae/src/main/resources/archetype-resources/src/main/java/gae/MyGaeInMemoryWokoInitListener.java
+++ archetype-gae/src/main/resources/archetype-resources/src/main/java/gae/MyGaeInMemoryWokoInitListener.java
@@ -9,15 +9,8 @@ import java.util.Arrays;
 import woko.inmemory.InMemoryUserManager;
 import woko.users.UserManager;
 
-import org.compass.annotations.Searchable;
-import org.compass.annotations.SearchableId;
-import org.compass.annotations.SearchableProperty;
-import org.hibernate.validator.NotNull;
-
-import javax.persistence.Entity;
-import javax.persistence.Id;
-
 import woko.gae.GaeWokoInitListener;
+
 
 public class MyGaeInMemoryWokoInitListener extends GaeWokoInitListener {
 
