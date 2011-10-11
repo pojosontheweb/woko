7a04f71dcd3a462376c0093b758a7f5c65ac7fbb
boissonnat <alexis.boissonnat@cstb.fr>
2011-10-11 10:22:05 +0200

    GAE impose to use String as ID..., change the ID property in MyEntity

--- archetype-gae/src/main/resources/archetype-resources/src/main/java/model/MyEntity.java
+++ archetype-gae/src/main/resources/archetype-resources/src/main/java/model/MyEntity.java
@@ -11,16 +11,16 @@ import javax.persistence.Id;
 public class MyEntity {
 
     @Id
-    private Long id;
+    private String id;
 
 
     private String myProp;
 
-    public Long getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(String id) {
         this.id = id;
     }
 
