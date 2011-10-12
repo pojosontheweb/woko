04c63a683f599c6f34d8872a682e50327376ad07
boissonnat <alexis.boissonnat@cstb.fr>
2011-10-12 18:03:53 +0200

    Use @NotNull from javax.validation since we use hb-validation v4

--- webtests/webtests-rpc/src/main/groovy/test/MyBook.groovy
+++ webtests/webtests-rpc/src/main/groovy/test/MyBook.groovy
@@ -2,10 +2,10 @@ package test
 
 import javax.persistence.Entity
 import javax.persistence.Id
-import org.hibernate.validator.NotNull
 import org.compass.annotations.SearchableProperty
 import org.compass.annotations.SearchableId
 import org.compass.annotations.Searchable
+import javax.validation.constraints.NotNull
 
 @Entity
 @Searchable
