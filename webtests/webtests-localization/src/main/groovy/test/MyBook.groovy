fb3cc251f813b8c00c545f9a565b25454bf5ec24
boissonnat <alexis.boissonnat@cstb.fr>
2011-10-12 17:35:06 +0200

    Add a Domain model object in Java to test hb-validator v4 (annotations doesn't work with groovy...)

--- webtests/webtests-localization/src/main/groovy/test/MyBook.groovy
+++ webtests/webtests-localization/src/main/groovy/test/MyBook.groovy
@@ -2,39 +2,43 @@ package test
 
 import javax.persistence.Entity
 import javax.persistence.Id
-import org.hibernate.validator.NotNull
 import org.compass.annotations.SearchableProperty
 import org.compass.annotations.SearchableId
 import org.compass.annotations.Searchable
+import javax.validation.constraints.NotNull
+import javax.persistence.GenerationType
+import javax.persistence.GeneratedValue
+import javax.validation.constraints.Min
 
 @Entity
 @Searchable
 class MyBook {
 
-  @NotNull
-  @SearchableProperty
-  String name
+    @NotNull
+    @SearchableProperty
+    String name
 
-  @Id
-  @SearchableId
-  String _id
+    @Id
+    @SearchableId
+    String _id
 
-  @SearchableProperty
-  int nbPages
+    @SearchableProperty
+    @Min((Long)2)
+    int nbPages
 
-  @SearchableProperty
-  Date creationTime = new Date()
+    @SearchableProperty
+    Date creationTime = new Date()
 
-  /*
-  transient List<MyBook> listOfMe = [this, this, this]
-  transient MyBook me = this
-  transient List<String> listOfStrings = ["abd", "ddd"]
+    /*
+    transient List<MyBook> listOfMe = [this, this, this]
+    transient MyBook me = this
+    transient List<String> listOfStrings = ["abd", "ddd"]
 
-  transient OtherPojo otherPojo = new OtherPojo()
-  transient List<OtherPojo> otherPojos = [new OtherPojo()]
+    transient OtherPojo otherPojo = new OtherPojo()
+    transient List<OtherPojo> otherPojos = [new OtherPojo()]
 
-  transient OtherPojo2 otherPojo2 = new OtherPojo2()
-  transient List<OtherPojo2> otherPojos2 = [new OtherPojo2()]
-  */
-  
+    transient OtherPojo2 otherPojo2 = new OtherPojo2()
+    transient List<OtherPojo2> otherPojos2 = [new OtherPojo2()]
+    */
+
 }
---
+++ webtests/webtests-localization/src/main/java/test/MyJavaEntity.java
@@ -0,0 +1,104 @@
+package test;
+
+import org.compass.annotations.Searchable;
+import org.compass.annotations.SearchableId;
+import org.compass.annotations.SearchableProperty;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Range;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ * @author Alexis Boissonnat - alexis.boissonnat 'at' gmail.com
+ */
+@Entity
+@Searchable
+public class MyJavaEntity {
+
+    @Id
+    @SearchableId
+    private Long id;
+
+    @NotNull
+    @SearchableProperty
+    private String name;
+
+    @Min(2)
+    @SearchableProperty
+    private int minNumber;
+
+    @Max(50)
+    @SearchableProperty
+    private int maxNumber;
+
+    @Size(min=2, max=10)
+    private String size;
+
+    @Range(min=2, max=10)
+    private int range;
+
+    @Email
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMinNumber() {
+        return minNumber;
+    }
+
+
+    public void setMinNumber(int minNumber) {
+        this.minNumber = minNumber;
+    }
+
+    public int getMaxNumber() {
+        return maxNumber;
+    }
+
+    public void setMaxNumber(int maxNumber) {
+        this.maxNumber = maxNumber;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public int getRange() {
+        return range;
+    }
+
+    public void setRange(int range) {
+        this.range = range;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
