1d54781fab67c38c815f41a7fdff8430de2777bb
Remi Vankeisbelck <remi@rvkb.com>
2012-02-08 01:14:37 +0100

    password prop excluded from user properties (user management ext)

---
+++ usermanagement/core/src/main/java/woko/ext/usermanagement/facets/RenderUserProperties.java
@@ -0,0 +1,21 @@
+package woko.ext.usermanagement.facets;
+
+import net.sourceforge.jfacets.annotations.FacetKey;
+import woko.ext.usermanagement.core.User;
+import woko.facets.builtin.all.RenderPropertiesImpl;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@FacetKey(name="renderProperties", profileId="all", targetObjectType = User.class)
+public class RenderUserProperties extends RenderPropertiesImpl {
+
+    @Override
+    public List<String> getPropertyNames() {
+        List<String> all = super.getPropertyNames();
+        ArrayList<String> withoutPassword = new ArrayList<String>(all);
+        withoutPassword.remove("password");
+        return Collections.unmodifiableList(withoutPassword);
+    }
+}
