628a19ae46a60eb3168b64f467f7278c3ea5c0f6
Remi Vankeisbelck <remi@rvkb.com>
2012-02-17 18:47:08 +0100

    resource bundles copy script to avoid hand-copy when we find errors in the prop files

---
+++ copyResources.groovy
@@ -0,0 +1,46 @@
+#!/usr/bin/env groovy
+
+def root = new File(".")
+def rootPath = root.absolutePath
+
+println "Starting in $rootPath..."
+
+def bundlesDir = "$rootPath/core/src/main/bundles"
+
+println "Grabbing the bundles text ($bundlesDir)"
+
+def locales = ["", "en", "fr"]
+def bundlesMap = [:]
+locales.each { l ->
+    def bName = l ? "WokoResources_${l}.properties" : "WokoResources.properties"
+    bundlesMap[bName] = new File(bundlesDir + File.separator + bName).text
+}
+
+
+def excludes = ["bundles", "target", ".git", "overlays"]
+
+println "Starting crawling files from $root.absolutePath..."
+
+int nbReplaced = 0
+
+new File('.').eachFileRecurse { f ->
+    def excluded = false
+    def fullPath = f.absolutePath
+    for (String s : excludes) {
+        if (fullPath.contains(s)) {
+            excluded = true
+            break
+        }
+    }
+    if (!excluded && fullPath.endsWith(".properties")) {
+        for (String s : bundlesMap.keySet()) {
+            if (fullPath.endsWith(s)) {
+                t.text = bundlesMap[s]
+                println "U $fullPath"
+                nbReplaced++
+            }
+        }
+    }
+}
+
+println "Done : replaced $nbReplaced files"
