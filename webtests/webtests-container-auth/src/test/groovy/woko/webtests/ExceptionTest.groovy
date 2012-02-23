4208eec2b36391cb803e76e06dfeb1516e4bffbb
Remi Vankeisbelck <remi@rvkb.com>
2012-02-23 21:51:30 +0100

    error pages with response codes - no look and feel, bare bones JSPs (closes #39)

--- webtests/webtests-container-auth/src/test/groovy/woko/webtests/ExceptionTest.groovy
+++ webtests/webtests-container-auth/src/test/groovy/woko/webtests/ExceptionTest.groovy
@@ -2,12 +2,31 @@ package woko.webtests
 
 class ExceptionTest extends WebTestBase {
 
-  void testException() {
-    webtest("testException") {
-        goToPage "/throw"
-        verifyText 'An error occured'
+
+
+    void testException500() {
+        webtest("testException500") {
+            config {
+                option name:"ThrowExceptionOnFailingStatusCode", value:false
+            }
+            goToPage "/throw"
+            storeResponseCode property:"status"
+            verifyProperty name:"status", text:"500"
+            verifyText 'An error occured'
+        }
     }
-  }
+
+    void testException404() {
+        webtest("testException404") {
+            config {
+                option name:"ThrowExceptionOnFailingStatusCode", value:false
+            }
+            goToPage "/dddbbb"
+            storeResponseCode property:"status"
+            verifyProperty name:"status", text:"404"
+            verifyText 'Page not found'
+        }
+    }
 
 
 }
