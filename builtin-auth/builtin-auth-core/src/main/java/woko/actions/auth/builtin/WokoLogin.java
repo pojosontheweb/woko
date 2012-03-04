7a36c10ebdd5ca252ab6d2a492a98c65400f5e17
Remi Vankeisbelck <remi@rvkb.com>
2012-03-04 14:42:19 +0100

    closes #67

--- builtin-auth/builtin-auth-core/src/main/java/woko/actions/auth/builtin/WokoLogin.java
+++ builtin-auth/builtin-auth-core/src/main/java/woko/actions/auth/builtin/WokoLogin.java
@@ -7,66 +7,68 @@ import woko.actions.BaseActionBean;
 import woko.actions.WokoActionBeanContext;
 import woko.util.WLogger;
 
+import javax.servlet.http.HttpServletRequest;
+
 @UrlBinding("/login")
 public class WokoLogin extends BaseActionBean {
 
-  private static final WLogger log = WLogger.getLogger(WokoLogin.class);
-  private static final String KEY_MSG_LOGIN_FAILED  = "woko.login.failed";
-  private static final String KEY_MSG_LOGIN_SUCCESS  = "woko.login.success";
+    private static final WLogger log = WLogger.getLogger(WokoLogin.class);
+    private static final String KEY_MSG_LOGIN_FAILED = "woko.login.failed";
+    private static final String KEY_MSG_LOGIN_SUCCESS = "woko.login.success";
 
-  public static final String SESSION_ATTR_CURRENT_USER = "__CURRENT_USER";
+    public static final String SESSION_ATTR_CURRENT_USER = "__CURRENT_USER";
 
-  @Validate(required = true)
-  private String username;
+    @Validate(required = true)
+    private String username;
 
-  private String targetUrl = "/home";
+    private String targetUrl = "/home";
 
-  public String getUsername() {
-    return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public String getTargetUrl() {
-    return targetUrl;
-  }
-
-  public void setTargetUrl(String targetUrl) {
-    this.targetUrl = targetUrl;
-  }
-
-  protected String authenticate() {
-    if (getContext().getWoko().getUserManager().authenticate(username, getContext().getRequest())) {
-      return username;
+    public String getUsername() {
+        return username;
     }
-    return null;
-  }
 
-  @DefaultHandler
-  @DontValidate
-  public Resolution displayForm() {
-      log.debug("Displaying login form");
-      return new ForwardResolution("/WEB-INF/woko/jsp/login.jsp");
-  }
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-  public Resolution login() {
-      log.debug("trying to log-in user " + username);
-      String user = authenticate();
-      if (user!=null) {
-          // auth OK, add message and redirect to the target URL
-          WokoActionBeanContext context = getContext();
-          context.getMessages().add(new LocalizableMessage(KEY_MSG_LOGIN_SUCCESS));
-          context.getRequest().getSession().setAttribute(SESSION_ATTR_CURRENT_USER, user);
-          log.debug(username + " logged in, redirecting to " + targetUrl);
-          return new RedirectResolution(targetUrl);
-      } else {
-          // authentication failed, add messages to context, and redirect to login
-          log.warn("Authentication failed for user '" + username + "', redirecting to login form again");
-          getContext().getValidationErrors().addGlobalError(new LocalizableError(KEY_MSG_LOGIN_FAILED));
-          return displayForm();
-      }
-  }
+    public String getTargetUrl() {
+        return targetUrl;
+    }
+
+    public void setTargetUrl(String targetUrl) {
+        this.targetUrl = targetUrl;
+    }
+
+    protected String authenticate() {
+        if (getContext().getWoko().getUserManager().authenticate(username, getContext().getRequest())) {
+            return username;
+        }
+        return null;
+    }
+
+    @DefaultHandler
+    @DontValidate
+    public Resolution displayForm() {
+        return new ForwardResolution("/WEB-INF/woko/jsp/login.jsp");
+    }
+
+    public Resolution login() {
+        log.debug("trying to log-in user " + username);
+        String user = authenticate();
+        if (user != null) {
+            // auth OK, add message and redirect to the target URL
+            WokoActionBeanContext context = getContext();
+            context.getMessages().add(new LocalizableMessage(KEY_MSG_LOGIN_SUCCESS));
+            context.getRequest().getSession().setAttribute(SESSION_ATTR_CURRENT_USER, user);
+            log.debug(username + " logged in, redirecting to " + targetUrl);
+            return new RedirectResolution(targetUrl);
+        } else {
+            // authentication failed, add messages to context, and redirect to login
+            log.warn("Authentication failed for user '" + username + "', redirecting to login form again");
+            getContext().getValidationErrors().addGlobalError(new LocalizableError(KEY_MSG_LOGIN_FAILED));
+            getContext().getResponse().setStatus(401);
+            return displayForm();
+        }
+    }
 
 }
