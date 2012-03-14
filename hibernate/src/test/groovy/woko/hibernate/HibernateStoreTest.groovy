14466f226c063e47127aa5eba139549543534c51
Remi Vankeisbelck <remi@rvkb.com>
2012-03-14 18:11:09 +0100

    refactor test to make use of doInTx

--- hibernate/src/test/groovy/woko/hibernate/HibernateStoreTest.groovy
+++ hibernate/src/test/groovy/woko/hibernate/HibernateStoreTest.groovy
@@ -24,60 +24,51 @@ import entities.MyEntity
 
 class HibernateStoreTest extends TestCase {
 
-  private HibernateStore store
+    private HibernateStore store
 
-  protected void setUp() {
-    store = new HibernateStore(['entities'])
-  }
+    protected void setUp() {
+        store = new HibernateStore(['entities'])
+    }
 
-  private void doInTx(Closure c) {
-    Session s = store.getSessionFactory().getCurrentSession()
-    Transaction tx = s.beginTransaction()
-    println "Started tx $tx"
-    try {
-      c.call(store)
-      println "Commiting  $tx"
-      tx.commit()
-    } catch(Throwable e) {
-      println "Exception caught : $e, Roll-backing and rethrowing"
-      tx.rollback()
-      throw new RuntimeException("Exception within tx", e)
+    private void doInTx(Closure c) {
+        store.doInTx({st, sess ->
+            c(st)
+        } as TxCallback)
     }
-  }
 
-  void testLoadNotExistingReturnsNull() {
-    doInTx { s ->
-      def e = s.load('MyEntity', '123')
-      assert e == null
+    void testLoadNotExistingReturnsNull() {
+        doInTx { s ->
+            def e = s.load('MyEntity', '123')
+            assert e == null
+        }
     }
-  }
 
-  void testSaveAndLoadAndDeleteInSameTx() {
-    doInTx { ObjectStore s ->
-      def e = new MyEntity([id:123,name:"Moby"])
-      s.save(e)
-      e = s.load("MyEntity", "123")
-      assert e.name == "Moby"
-      s.delete(e)
-      e = s.load("MyEntity", "123")
-      assert e == null
+    void testSaveAndLoadAndDeleteInSameTx() {
+        doInTx { ObjectStore s ->
+            def e = new MyEntity([id: 123, name: "Moby"])
+            s.save(e)
+            e = s.load("MyEntity", "123")
+            assert e.name == "Moby"
+            s.delete(e)
+            e = s.load("MyEntity", "123")
+            assert e == null
+        }
     }
-  }
 
-  void testSaveAndLoadAndDeleteInDifferentTx() {
-    doInTx { s ->
-      def e = new MyEntity([id:123,name:"Moby"])
-      s.save(e)
+    void testSaveAndLoadAndDeleteInDifferentTx() {
+        doInTx { s ->
+            def e = new MyEntity([id: 123, name: "Moby"])
+            s.save(e)
+        }
+        doInTx { s ->
+            def e = s.load("MyEntity", "123")
+            assert e.name == "Moby"
+            s.delete(e)
+        }
+        doInTx { s ->
+            def e = s.load("MyEntity", "123")
+            assert e == null
+        }
     }
-    doInTx { s ->
-      def e = s.load("MyEntity", "123")
-      assert e.name == "Moby"
-      s.delete(e)
-    }
-    doInTx { s ->
-      def e = s.load("MyEntity", "123")
-      assert e == null
-    }
-  }
 
 }
