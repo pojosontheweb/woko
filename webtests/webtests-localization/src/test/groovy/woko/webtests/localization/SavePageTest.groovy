09afa326ce60680f96bbb6f86197853b125092c1
boissonnat <alexis.boissonnat@gmail.com>
2012-03-15 10:55:46 +0100

    Clean Localization tests

--- webtests/webtests-localization/src/test/groovy/woko/webtests/localization/SavePageTest.groovy
+++ webtests/webtests-localization/src/test/groovy/woko/webtests/localization/SavePageTest.groovy
@@ -30,9 +30,9 @@ class SavePageTest extends WebTestBase{
             verifyXPath xpath: "/html/body/div/div[3]/div/div[1]/span", text:'Please fix the following errors:'
 
             // Check if the 'cannot be null' hibernate error has been transformed in 'FieldName is a required field' Stripes error
-            verifyXPath xpath: "/html/body/div/div[3]/div/div[1]/ul/li[1]", text:'The minimum allowed value for Object Min Number is 2'
+            verifyXPath xpath: "/html/body/div/div[3]/div/div[1]/ul/li[1]", text:'The minimum allowed value for Min is 2'
             verifyXPath xpath: "/html/body/div/div[3]/div/div[1]/ul/li[2]", text:'Name is a required field'
-            verifyXPath xpath: "/html/body/div/div[3]/div/div[1]/ul/li[3]", text:'Object Range must be between 2 and 10'
+            verifyXPath xpath: "/html/body/div/div[3]/div/div[1]/ul/li[3]", text:'Range must be between 2 and 10'
         }
     }
 }
