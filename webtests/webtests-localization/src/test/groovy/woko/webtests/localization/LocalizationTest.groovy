7601cef0734defb3a242954fa8dcb58d9184dac7
Remi Vankeisbelck <remi@rvkb.com>
2012-03-19 11:00:48 +0100

    failing test for ???xyz??? messages

--- webtests/webtests-localization/src/test/groovy/woko/webtests/localization/LocalizationTest.groovy
+++ webtests/webtests-localization/src/test/groovy/woko/webtests/localization/LocalizationTest.groovy
@@ -29,4 +29,15 @@ class LocalizationTest extends WebTestBase {
             verifyText 'Woko message (woko.guest.home.content) overridden from application resources'
         }
     }
+
+    void testMissingKeysDontGenerateUglyLabels() {
+        webtest("testMissingKeysDontGenerateUglyLabels") {
+            login()
+            goToPage '/save/MyBook'
+            verifyText "Number of pages"
+            not {
+                verifyText "???MyBook.anotherProp???" // the message doesn't exist for the prop, we check if we don't have an ugly ???xyz??? message
+            }
+        }
+    }
 }
