bb3fbb9f5ab498a5ffb775722489cbd1a94b1c69
Mathieu <mathieu@marache.com>
2012-03-20 15:16:00 +0100

    corrected windows side of the script #94

--- tooling/src/main/scripts/woko.bat
+++ tooling/src/main/scripts/woko.bat
@@ -8,7 +8,7 @@ if exist pom.xml (
     echo  \ \/ \/ // o \^|   K /   \
     echo   \__W__/ \___/^|_^|\_\\_o_/  2.0
     echo              POJOs on the Web !
-    echo
+    echo.
 
     if "%1" == "init" (
         shift
@@ -17,31 +17,32 @@ if exist pom.xml (
 		:input
 		set INPUT=
 		set /P INPUT=Do you want to init the project ? [n]: %=%
-		if "%INPUT%"!="y" (
+		if not "%INPUT%" == "y" (
             echo Nothing done. Hope to see you soon !
-            exit 0
+            goto:EOF 
 		)
     )
 
     set ARTIFACTID=
     set GROUPID=
     set VERSION=
-    set GROOVY="no"
-    set BOOTSTRAP="no"
+    set GROOVY=no
+    set BOOTSTRAP=no
     set PACKAGE=
 
     :GETOPTS
-        if /I %1 == -h goto Help
-        if /I %1 == -n set ARTIFACTID=%2& shift
-        if /I %1 == -m set GROUPID=%2& shift
-        if /I %1 == -v set VERSION=%2& shift
-        if /I %1 == -p set PACKAGE=%2& shift
-        if /I %1 == -g set GROOVY="yes"
-        if /I %1 == -b set BOOTSTRAP="yes"
+        if /I "%1" == "-h" goto Help
+        if /I "%1" == "-n" set ARTIFACTID=%~2& shift
+        if /I "%1" == "-m" set GROUPID=%~2& shift
+        if /I "%1" == "-v" set VERSION=%~2& shift
+        if /I "%1" == "-p" set PACKAGE=%~2& shift
+        if /I "%1" == "-g" set GROOVY=yes
+        if /I "%1" == "-b" set BOOTSTRAP=yes
         shift
-    if not (%1)==() goto GETOPTS
+		
+    if not "%1"=="" GOTO GETOPTS
 
-    echo Initializing project
+	echo Initializing project
 
     if "X%ARTIFACTID%" == "X" (
         set /P ARTIFACTID=Project name ? : %=%
@@ -52,7 +53,7 @@ if exist pom.xml (
     )
 
     if "X%VERSION%" == "X" (
-        set VERSION="1.0-SNAPSHOT"
+        set VERSION=1.0-SNAPSHOT
         set /P VERSION=Your project's version ? [1.0-SNAPSHOT] : %=%
     )
 
@@ -60,22 +61,21 @@ if exist pom.xml (
         set PACKAGE=%GROUPID%
     )
 
-    echo "| Generating your project, please wait, it can take a while to download everything..."
-    mvn archetype:generate -DarchetypeArtifactId=woko-archetype -DarchetypeGroupId=com.rvkb\
-                    -DgroupId="%GROUPID%" -DartifactId="%ARTIFACTID%" -Dversion="%VERSION%" -DinteractiveMode="false" -q
+    echo ^| Generating your project, please wait, it can take a while to download everything...
+    mvn archetype:generate -DarchetypeArtifactId=woko-archetype -DarchetypeGroupId=com.rvkb -DgroupId="%GROUPID%" -DartifactId="%ARTIFACTID%" -Dversion="%VERSION%" -DinteractiveMode="false" -q
 
     cd "%ARTIFACTID%"
     mvn exec:java -Dexec.mainClass="woko.tooling.cli.Runner" -Dexec.classpathScope=runtime -Dexec.args="generate -b %BOOTSTRAP% -g %GROOVY% -p %PACKAGE%" -q
 
-    exit 0
-
+    goto:EOF
+	
     :Help
-        echo Usage: woko init"
-        echo " -n project name, serves as the artifact Id (i.e. myapp)"
-        echo " -m the maven group Id (i.e. com.myexample.myapp)"
-        echo " -p the default package name, defaults to maven group Id"
-        echo " -v the version (i.e. 1.0-SNAPSHOT)"
-        echo " -b use Boostrap css & js, defaults to no"
-        echo " -g use Groovy, defaults to no"
+        echo Usage: woko init
+        echo  -n project name, serves as the artifact Id (i.e. myapp)
+        echo  -m the maven group Id (i.e. com.myexample.myapp)
+        echo  -p the default package name, defaults to maven group Id
+        echo  -v the version (i.e. 1.0-SNAPSHOT)
+        echo  -b use Boostrap css ^& js, defaults to no
+        echo  -g use Groovy, defaults to no
 
 )
