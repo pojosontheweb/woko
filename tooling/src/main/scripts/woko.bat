40e3528f40f5025d5134ab9fdbaff4f1f6b129df
Mathieu <mathieu@marache.com>
2012-03-21 17:46:45 +0100

    updated both scripts

--- tooling/src/main/scripts/woko.bat
+++ tooling/src/main/scripts/woko.bat
@@ -26,9 +26,10 @@ if exist pom.xml (
     set ARTIFACTID=
     set GROUPID=
     set VERSION=
-    set GROOVY=yes
-    set BOOTSTRAP=yes
+    set GROOVY=
+    set BOOTSTRAP=
     set PACKAGE=
+    set EXECARGS="generate"
 
     :GETOPTS
         if /I "%1" == "-h" goto Help
@@ -37,35 +38,46 @@ if exist pom.xml (
         if /I "%1" == "-v" set VERSION=%~2& shift
         if /I "%1" == "-p" set PACKAGE=%~2& shift
         if /I "%1" == "-g" set GROOVY=yes
+        if /I "%1" == "-j" set GROOVY=no
         if /I "%1" == "-b" set BOOTSTRAP=yes
+        if /I "%1" == "-l" set BOOTSTRAP=no
         shift
 		
     if not "%1"=="" GOTO GETOPTS
 
-            echo Initializing project
+    echo Initializing project
 
     if "X%ARTIFACTID%" == "X" (
-            set /P ARTIFACTID=Project name ? : %=%
+        set /P ARTIFACTID=Project name ? : %=%
     )
 
     if "X%GROUPID%" == "X" (
-            set /P GROUPID=Maven groupId ? : %=%
+        set /P GROUPID=Maven groupId ? : %=%
     )
 
     if "X%VERSION%" == "X" (
         set VERSION=1.0-SNAPSHOT
-            set /P VERSION=Your project's version ? [1.0-SNAPSHOT] : %=%
+        set /P VERSION=Your project's version ? [1.0-SNAPSHOT] : %=%
     )
 
-    if "X%PACKAGE%" == "X" (
-        set PACKAGE=%GROUPID%
+    if not "X%GROOVY%" == "X" (
+        set EXECARGS="%EXECARGS% -g %GROOVY%"
+    )
+
+    if not "X%BOOTSTRAP%" == "X" (
+        set EXECARGS="%EXECARGS% -b %BOOTSTRAP%"
+    )
+
+
+    if not "X%PACKAGE%" == "X" (
+        set EXECARGS="%EXECARGS% -p %PACKAGE%"
     )
 
     echo ^| Generating your project, please wait, it can take a while to download everything...
     mvn archetype:generate -DarchetypeArtifactId=woko-archetype -DarchetypeGroupId=com.rvkb -DgroupId="%GROUPID%" -DartifactId="%ARTIFACTID%" -Dversion="%VERSION%" -DinteractiveMode="false" -q
 
     cd "%ARTIFACTID%"
-    mvn exec:java -Dexec.mainClass="woko.tooling.cli.Runner" -Dexec.classpathScope=runtime -Dexec.args="generate -b %BOOTSTRAP% -g %GROOVY% -p %PACKAGE%" -q
+    mvn exec:java -Dexec.mainClass="woko.tooling.cli.Runner" -Dexec.classpathScope=runtime -Dexec.args=%EXECARGS% -q
 
     goto:EOF
 	
@@ -75,7 +87,10 @@ if exist pom.xml (
         echo  -m the maven group Id (i.e. com.myexample.myapp)
         echo  -p the default package name, defaults to maven group Id
         echo  -v the version (i.e. 1.0-SNAPSHOT)
-        echo  -b don't use Boostrap css ^& js
-        echo  -g don't use Groovy
+        echo  -b use Boostrap css ^& js
+        echo  -l use lithium skin
+        echo  -g use Groovy
+        echo  -j use pure java
+        echo  -h this help
 
 )
