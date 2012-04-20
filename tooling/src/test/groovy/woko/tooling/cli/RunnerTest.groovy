acb6caef0fb20562a6c6905d7eeb1f9d405869d0
Remi Vankeisbelck <remi@rvkb.com>
2012-04-20 19:18:37 +0200

    fix runner test (one more facet)

--- tooling/src/test/groovy/woko/tooling/cli/RunnerTest.groovy
+++ tooling/src/test/groovy/woko/tooling/cli/RunnerTest.groovy
@@ -100,7 +100,7 @@ The command accepts one argument that can be  :
 
     @Test
     void testListFacets() {
-        assertCommandResult(["list", "facets"], """41 facets found : \n  create, developer, java.lang.Object, woko.facets.builtin.developer.Create
+        assertCommandResult(["list", "facets"], """42 facets found : \n  create, developer, java.lang.Object, woko.facets.builtin.developer.Create
   delete, developer, java.lang.Object, woko.facets.builtin.developer.DeleteImpl
   edit, developer, java.lang.Object, woko.facets.builtin.developer.EditImpl
   find, developer, java.lang.Object, woko.facets.builtin.developer.Find
@@ -126,6 +126,7 @@ The command accepts one argument that can be  :
   renderPropertyValue, all, java.util.Date, woko.facets.builtin.all.RenderPropertyValueDate
   renderPropertyValue, all, java.util.Collection, woko.facets.builtin.all.RenderPropertyValueCollection
   renderPropertyValueEdit, all, java.lang.Number, woko.facets.builtin.all.RenderPropertyValueEditStripesText
+  renderPropertyValueEdit, all, java.lang.Object, woko.facets.builtin.all.RenderPropertyValueEditXToOneRelation
   renderPropertyValueEdit, all, java.lang.String, woko.facets.builtin.all.RenderPropertyValueEditStripesText
   renderPropertyValueEdit, all, java.util.Date, woko.facets.builtin.all.RenderPropertyValueEditDate
   renderPropertyValueJson, all, java.lang.Boolean, woko.facets.builtin.all.RenderPropertyValueJsonBasicTypes
