75796ffd47ee4e35abbea295051ab9d307ade7a7
Remi Vankeisbelck <remi@rvkb.com>
2013-01-23 16:31:32 +0100

    #170 : start prop value override, TBC : maybe add an AJAX timer to show refresh of some field.

---
+++ webtests/webtests-async/src/main/groovy/webtests/async/ListJobMyDetailsGuest.groovy
@@ -0,0 +1,11 @@
+package webtests.async
+
+import net.sourceforge.jfacets.annotations.FacetKey
+import woko.facets.builtin.developer.ListTabularImpl
+
+@FacetKey(name="list", profileId = "guest", targetObjectType = MyJobDetails.class)
+class ListJobMyDetailsGuest extends ListTabularImpl {
+
+
+
+}
