28ffe8afe6888874026e1c08f2f2ef1274f0e104
Remi Vankeisbelck <remi@rvkb.com>
2013-01-28 11:48:51 +0100

    #166 : fix response headers and filter chain order

--- core/src/main/java/woko/util/httpcaching/WokoHttpCacheFilter.java
+++ core/src/main/java/woko/util/httpcaching/WokoHttpCacheFilter.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 
 public class WokoHttpCacheFilter implements Filter {
 
-    private static final long YEAR = 1000L * 3600L * 24L * 360L;
+    private static final long DELAY = 1000L * 3600L * 24L * 100L; // 100 days
     public static final String APP_CTX_ATTR_NAME = "wokoHttpCacheFilter";
 
     private String cacheTokenValue;
@@ -24,7 +24,6 @@ public class WokoHttpCacheFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        chain.doFilter(request, response);
         // check that cache token is provided in request, and that it is equals to the token we
         // have for the app, so that we cache only what really meant to be cached and
         // disable caching for "random" values of the cache token.
@@ -34,9 +33,12 @@ public class WokoHttpCacheFilter implements Filter {
             // cached resource, mark HTTP response headers as such
             long now = System.currentTimeMillis();
             HttpServletResponse resp = (HttpServletResponse)response;
-            resp.setDateHeader("Expires", now + YEAR);
-            resp.setDateHeader("Last-Modified", now - YEAR);
+            resp.setDateHeader("Expires", now + DELAY);
+            resp.setDateHeader("Last-Modified", now - DELAY);
+            resp.setHeader("Cache-Control", "public, max-age=3153600");
         }
+
+        chain.doFilter(request, response);
     }
 
     public String getCacheTokenValue() {
