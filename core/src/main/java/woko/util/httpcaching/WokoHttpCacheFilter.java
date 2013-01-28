95e82fbcb0f78efa2127b0422fc5e647242d3059
Remi Vankeisbelck <remi@rvkb.com>
2013-01-28 12:15:20 +0100

    #166 : polish

--- core/src/main/java/woko/util/httpcaching/WokoHttpCacheFilter.java
+++ core/src/main/java/woko/util/httpcaching/WokoHttpCacheFilter.java
@@ -1,23 +1,81 @@
+/*
+ * Copyright 2001-2013 Remi Vankeisbelck
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package woko.util.httpcaching;
 
+import woko.util.WLogger;
+
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * Request filter that helps browser caching by using a cacheToken
+ * in the request parameters.
+ * Automatically sets response headers for never-expiring resources.
+ *
+ * cacheToken request parameter and value are configurable.
+ */
 public class WokoHttpCacheFilter implements Filter {
 
     private static final long DELAY = 1000L * 3600L * 24L * 100L; // 100 days
     public static final String APP_CTX_ATTR_NAME = "wokoHttpCacheFilter";
+    public static final String CACHE_TOKEN_DEFAULT_PARAM_NAME = "cacheToken";
+    public static final String CFG_PARAM_NAME = "cacheTokenParamName";
+    public static final String CFG_TOKEN_VALUE = "cacheTokenValue";
+    public static final String HEADER_EXPIRES = "Expires";
+    public static final String HEADER_LAST_MODIFIED = "Last-Modified";
+    public static final String HEADER_CACHE_CONTROL = "Cache-Control";
+    public static final String CACHE_CONTROL_VALUE = "public, max-age=3153600";
 
-    private String cacheTokenValue;
-    private String cacheTokenParamName = "cacheToken";
+    private static final WLogger logger = WLogger.getLogger(WokoHttpCacheFilter.class);
 
+    private String cacheTokenValue = Long.toString(System.currentTimeMillis());
+    private String cacheTokenParamName = CACHE_TOKEN_DEFAULT_PARAM_NAME;
+
+    /**
+     * Initialize the filter with values from config if any. Fallbacks to default
+     * values if no init params provided in filter config.
+     * Binds this to the servletContext so that the filter can be accessed
+     * from the app.
+     * @param filterConfig the filter config
+     * @throws ServletException
+     */
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        cacheTokenValue = Long.toString(System.currentTimeMillis());
+        String configParamName = filterConfig.getInitParameter(CFG_PARAM_NAME);
+        if (configParamName!=null) {
+            cacheTokenParamName = configParamName;
+        }
+        String configTokenValue = filterConfig.getInitParameter(CFG_TOKEN_VALUE);
+        if (configTokenValue!=null) {
+            cacheTokenValue = configTokenValue;
+        }
         filterConfig.getServletContext().setAttribute(APP_CTX_ATTR_NAME, this);
+        logger.info("Initialized :");
+        logger.info("  * cacheTokenParamName = " + getCacheTokenParamName());
+        logger.info("  * cacheTokenValue = " + getCacheTokenValue());
     }
 
+    /**
+     * Return the filter (if any) for passed servlet context.
+     * @param ctx the servlet context
+     * @return the filter instance
+     */
     public static WokoHttpCacheFilter get(ServletContext ctx) {
         return (WokoHttpCacheFilter)ctx.getAttribute(APP_CTX_ATTR_NAME);
     }
@@ -30,12 +88,17 @@ public class WokoHttpCacheFilter implements Filter {
         String requestCacheToken = request.getParameter(getCacheTokenParamName());
         if (requestCacheToken!=null && requestCacheToken.equals(getCacheTokenValue())) {
 
+            if (logger.isDebugEnabled()) {
+                HttpServletRequest req = (HttpServletRequest)request;
+                logger.debug("Applying headers for request URI=" + req.getRequestURI() + " queryString=" + req.getQueryString());
+            }
+
             // cached resource, mark HTTP response headers as such
             long now = System.currentTimeMillis();
             HttpServletResponse resp = (HttpServletResponse)response;
-            resp.setDateHeader("Expires", now + DELAY);
-            resp.setDateHeader("Last-Modified", now - DELAY);
-            resp.setHeader("Cache-Control", "public, max-age=3153600");
+            resp.setDateHeader(HEADER_EXPIRES, now + DELAY);
+            resp.setDateHeader(HEADER_LAST_MODIFIED, now - DELAY);
+            resp.setHeader(HEADER_CACHE_CONTROL, CACHE_CONTROL_VALUE);
         }
 
         chain.doFilter(request, response);
@@ -49,8 +112,15 @@ public class WokoHttpCacheFilter implements Filter {
         return cacheTokenParamName;
     }
 
+    public void setCacheTokenValue(String cacheTokenValue) {
+        this.cacheTokenValue = cacheTokenValue;
+    }
+
+    public void setCacheTokenParamName(String cacheTokenParamName) {
+        this.cacheTokenParamName = cacheTokenParamName;
+    }
+
     @Override
     public void destroy() {
-
     }
 }
