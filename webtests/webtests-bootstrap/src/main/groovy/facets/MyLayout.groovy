e2cbf291d3e534efb4d01a62668bbe3ccdb440ea
Alexis Boissonnat <alexis.boissonnat@gmail.com>
2013-02-21 14:55:37 +0100

    Move CSS import from jsp to the layout facet

---
+++ webtests/webtests-bootstrap/src/main/groovy/facets/MyLayout.groovy
@@ -0,0 +1,23 @@
+package facets
+
+import net.sourceforge.jfacets.annotations.FacetKey
+import woko.facets.builtin.WokoFacets
+import woko.facets.builtin.all.LayoutAll
+
+@FacetKey(name= WokoFacets.layout, profileId="all")
+class MyLayout extends LayoutAll {
+
+    @Override
+    String getAppTitle() {
+        return "Bootstrap Skin"
+    }
+
+    @Override
+    List<String> getCssIncludes() {
+        return [
+                "/css/bootstrap-v2.3.0/bootstrap.css",
+                "/css/responsive.css",
+                "/css/woko.css"
+        ]
+    }
+}
