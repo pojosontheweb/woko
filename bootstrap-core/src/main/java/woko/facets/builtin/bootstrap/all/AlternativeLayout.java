f364faacd3bd5a04d5632f6405b97da463bd9639
Alexis Boissonnat <alexis.boissonnat@gmail.com>
2013-02-25 17:23:44 +0100

    Add image, description and crappy HowTo

--- bootstrap-core/src/main/java/woko/facets/builtin/bootstrap/all/AlternativeLayout.java
+++ bootstrap-core/src/main/java/woko/facets/builtin/bootstrap/all/AlternativeLayout.java
@@ -2,6 +2,32 @@ package woko.facets.builtin.bootstrap.all;
 
 public enum AlternativeLayout {
 
-    fluid,
+    fluid(
+            "http://twitter.github.com/bootstrap/assets/img/examples/bootstrap-example-fluid.png",
+            "Create a fluid, two-column page. Great for applications and docs."
+    );
 
+    private String imgUrl;
+    private String description;
+
+    private AlternativeLayout(String imgUrl, String description) {
+        this.imgUrl = imgUrl;
+        this.description = description;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
