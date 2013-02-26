3dc12214512904f28ef1549f5050637c6ef1d7fa
Remi Vankeisbelck <remi@rvkb.com>
2013-02-26 14:52:47 +0100

    version 2.2-beta3

--- core/src/main/java/woko/Woko.java
+++ core/src/main/java/woko/Woko.java
@@ -102,7 +102,7 @@ public class Woko<
     /**
      * Woko version
      */
-    public static final String VERSION = "LATEST-SNAPSHOT";
+    public static final String VERSION = "2.2-beta3";
 
     /**
      * Return the Woko instance for passed servletContext
