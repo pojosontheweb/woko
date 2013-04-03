da4b81dc7d95c4e93b22fa9dfecfe955dee8c928
Remi Vankeisbelck <remi@rvkb.com>
2013-04-03 19:36:46 +0200

    bump-version script (more poms)

--- bump-version.groovy
+++ bump-version.groovy
@@ -29,7 +29,7 @@ new File(".").eachFileRecurse { File f ->
     }
 }
 
-assert nbPoms == 45
+assert nbPoms == 48
 println "Replaced versions in $nbPoms pom files"
 
 println "Handling bash scripts..."
