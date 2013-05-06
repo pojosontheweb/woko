6ebe4df842f8b92de57c99be56775c92913f0d8e
Remi Vankeisbelck <remi@rvkb.com>
2013-05-06 19:27:06 +0200

    fix nb poms check in bump-version.sh (new pom for TinyMCE4)

--- bump-version.groovy
+++ bump-version.groovy
@@ -29,7 +29,7 @@ new File(".").eachFileRecurse { File f ->
     }
 }
 
-assert nbPoms == 48
+assert nbPoms == 49 
 println "Replaced versions in $nbPoms pom files"
 
 println "Handling bash scripts..."
