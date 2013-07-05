d0718308d1cb5bda7104124a6dff87cb3ca5dade
Remi Vankeisbelck <remi@rvkb.com>
2013-07-05 14:36:48 +0200

    fix missing layout for purecss webtests

---
+++ webtests/webtests-purecss/src/main/groovy/facets/MyPurecssLayout.groovy
@@ -0,0 +1,8 @@
+package facets
+
+import net.sourceforge.jfacets.annotations.FacetKey
+import woko.facets.builtin.purecss.all.LayoutPurecss
+
+@FacetKey(name="layout", profileId = "all")
+class MyPurecssLayout extends LayoutPurecss {
+}
