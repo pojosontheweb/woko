ca076385657cc71d46ae44495dc83ee7f8086ef5
Remi Vankeisbelck <remi@rvkb.com>
2014-04-26 10:51:23 +0200

    2.3.2 : bump version

--- core/src/main/java/woko/Woko.java
+++ core/src/main/java/woko/Woko.java
@@ -103,7 +103,7 @@ public class Woko<
     /**
      * Woko version
      */
-    public static final String VERSION = "2.3.1";
+    public static final String VERSION = "2.3.2";
 
     /**
      * Return the Woko instance for passed servletContext
