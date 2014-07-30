bd9c51180b3e9b6a7013fa5660782ca63bc2d229
Remi Vankeisbelck <remi@rvkb.com>
2014-07-30 19:35:43 +0200

    return 403 in RPC mode for LoginRequiredFacet

--- builtin-auth/builtin-auth-core/src/main/java/woko/facets/builtin/auth/LoginRequiredFacet.java
+++ builtin-auth/builtin-auth-core/src/main/java/woko/facets/builtin/auth/LoginRequiredFacet.java
@@ -6,8 +6,12 @@ import net.sourceforge.stripes.action.ActionBeanContext;
 import net.sourceforge.stripes.action.ForwardResolution;
 import net.sourceforge.stripes.action.Resolution;
 import net.sourceforge.stripes.action.StrictBinding;
+import net.sourceforge.stripes.rpc.RpcResolutionWrapper;
+import org.json.JSONException;
+import org.json.JSONObject;
 import woko.facets.BaseResolutionFacet;
 import woko.facets.builtin.WokoFacets;
+import woko.util.JsonResolution;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -19,35 +23,48 @@ import javax.servlet.http.HttpServletRequest;
  * You can override your own facets for your own role so that guest users are
  * forced to login when they try to reach some facets, instead of getting a 404 (FacetNotFound).
  */
-@FacetKeyList(keys ={
-    @FacetKey(name = WokoFacets.view, profileId = "guest"),
-    @FacetKey(name = WokoFacets.edit, profileId = "guest"),
-    @FacetKey(name = WokoFacets.create, profileId = "guest"),
-    @FacetKey(name = WokoFacets.delete, profileId = "guest"),
-    @FacetKey(name = WokoFacets.save, profileId = "guest"),
-    @FacetKey(name = WokoFacets.json, profileId = "guest"),
-    @FacetKey(name = WokoFacets.find, profileId = "guest"),
-    @FacetKey(name = WokoFacets.list, profileId = "guest"),
-    @FacetKey(name = WokoFacets.search, profileId = "all"),
-    @FacetKey(name = WokoFacets.studio, profileId = "guest")
+@FacetKeyList(keys = {
+        @FacetKey(name = WokoFacets.view, profileId = "guest"),
+        @FacetKey(name = WokoFacets.edit, profileId = "guest"),
+        @FacetKey(name = WokoFacets.create, profileId = "guest"),
+        @FacetKey(name = WokoFacets.delete, profileId = "guest"),
+        @FacetKey(name = WokoFacets.save, profileId = "guest"),
+        @FacetKey(name = WokoFacets.json, profileId = "guest"),
+        @FacetKey(name = WokoFacets.find, profileId = "guest"),
+        @FacetKey(name = WokoFacets.list, profileId = "guest"),
+        @FacetKey(name = WokoFacets.search, profileId = "all"),
+        @FacetKey(name = WokoFacets.studio, profileId = "guest")
 })
 @StrictBinding
 public class LoginRequiredFacet extends BaseResolutionFacet {
 
-  public Resolution getResolution(ActionBeanContext abc) {
-    HttpServletRequest request = abc.getRequest();
-    String requestedPage = request.getRequestURI();
-    // strip context path if needed
-    String contextPath = request.getContextPath();
-    if (requestedPage.startsWith(contextPath)) {
-      requestedPage = requestedPage.substring(contextPath.length());
+    public Resolution getResolution(final ActionBeanContext abc) {
+        HttpServletRequest request = abc.getRequest();
+        String requestedPage = request.getRequestURI();
+        // strip context path if needed
+        String contextPath = request.getContextPath();
+        if (requestedPage.startsWith(contextPath)) {
+            requestedPage = requestedPage.substring(contextPath.length());
+        }
+        String queryString = request.getQueryString();
+        if (queryString != null) {
+            requestedPage += "?" + queryString;
+        }
+        return new RpcResolutionWrapper(new ForwardResolution("/login").addParameter("targetUrl", requestedPage)) {
+            @Override
+            public Resolution getRpcResolution() {
+                JSONObject result = new JSONObject();
+                try {
+                    result.put("error", true);
+                    result.put("forbidden", true);
+                } catch(JSONException e) {
+                    throw new RuntimeException(e);
+                }
+                abc.getResponse().setStatus(403);
+                return new JsonResolution(result);
+            }
+        };
     }
-    String queryString = request.getQueryString();
-    if (queryString != null) {
-      requestedPage += "?" + queryString;
-    }
-    return new ForwardResolution("/login").addParameter("targetUrl", requestedPage);
-  }
 
 
 }
