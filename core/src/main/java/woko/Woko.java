044a02bac4b3d14acbe1b199ea2fe0e207ca22d6
Remi Vankeisbelck <remi@rvkb.com>
2015-03-08 11:34:35 +0100

    LATEST-SNAP

--- core/src/main/java/woko/Woko.java
+++ core/src/main/java/woko/Woko.java
@@ -104,7 +104,7 @@ public class Woko<
     /**
      * Woko version
      */
-    public static final String VERSION = "2.4-beta";
+    public static final String VERSION = "LATEST-SNAPSHOT";
 
     /**
      * Return the Woko instance for passed servletContext
