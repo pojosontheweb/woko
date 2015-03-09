485b97b3aba8fb45e3b21bba6156e5b9428c3519
Remi Vankeisbelck <remi@rvkb.com>
2015-03-09 13:50:19 +0100

    refactor woko.resolutions() to use singleton

---
+++ core/src/main/java/woko/util/JSON.java
@@ -0,0 +1,27 @@
+package woko.util;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.List;
+import java.util.Map;
+
+public class JSON {
+
+    public static JSONObject toJSONObject(Map<String,?> map) {
+
+    }
+
+    public static JSONArray toJSONArray(List<?> list) {
+
+    }
+
+    public static Map<String,?> toMap(JSONObject jsonObject) {
+
+    }
+
+    public static List<?> toList(JSONArray jsonArray) {
+
+    }
+
+}
