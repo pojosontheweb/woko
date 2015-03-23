050151c533123358d0c9f3abdfad38d1ad146445
Remi Vankeisbelck <remi@rvkb.com>
2015-03-23 15:11:36 +0100

    fix #225 : doc

--- core/src/main/java/woko/ioc/WokoInject.java
+++ core/src/main/java/woko/ioc/WokoInject.java
@@ -6,12 +6,17 @@ import java.lang.annotation.*;
  * Annotation that can be placed on facets, in order to auto-inject components
  * from the IoC.
  * Can be used on public "setter methods", or private fields with a setter.
+ *
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.FIELD})
 @Documented
 public @interface WokoInject {
 
+    /**
+     * Key of the component to be injected. If null, auto-wiring (by type) will be used.
+     * @return the key of the component in the ioc container, or null
+     */
     String value() default "";
 
 }
