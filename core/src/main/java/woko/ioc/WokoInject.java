df5399babeb97be142c500aa1fadf7f2ce1bfc9f
Remi Vankeisbelck <remi@rvkb.com>
2015-03-23 14:20:13 +0100

    #225 : @WokoInject on fields. Makes it easier to use with Groovy

--- core/src/main/java/woko/ioc/WokoInject.java
+++ core/src/main/java/woko/ioc/WokoInject.java
@@ -3,12 +3,12 @@ package woko.ioc;
 import java.lang.annotation.*;
 
 /**
- * Annotation that can be placed on facet methods in order to auto-inject components
- * from the IoC into facets.
- * Must be used on public methods that accept only a parameter (the component required).
+ * Annotation that can be placed on facets, in order to auto-inject components
+ * from the IoC.
+ * Can be used on public "setter methods", or private fields with a setter.
  */
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.METHOD})
+@Target({ElementType.METHOD, ElementType.FIELD})
 @Documented
 public @interface WokoInject {
 
--- core/src/main/java/woko/ioc/WokoInjectHelper.java
+++ core/src/main/java/woko/ioc/WokoInjectHelper.java
@@ -4,7 +4,10 @@ import net.sourceforge.stripes.util.ReflectUtil;
 import woko.Woko;
 import woko.util.WLogger;
 
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.Collection;
 
 public class WokoInjectHelper {
 
@@ -13,18 +16,19 @@ public class WokoInjectHelper {
     public static void injectComponents(Woko<?,?,?,?> woko, Object facet) {
         // iterate on public methods and find annotated
         // with @WokoInject
-        for (Method m : ReflectUtil.getMethods(facet.getClass())) {
+        Class<?> clazz = facet.getClass();
+        for (Method m : ReflectUtil.getMethods(clazz)) {
             WokoInject wokoInject = m.getAnnotation(WokoInject.class);
             if (wokoInject!=null) {
                 if (logger.isDebugEnabled()) {
-                    logger.debug("Found method with @WokoInject : " + facet.getClass().getName() + "#" + m.getName());
+                    logger.debug("Found method with @WokoInject : " + clazz.getName() + "#" + m.getName());
                 }
                 // annotation found, retrieve the service...
                 String componentKey = wokoInject.value();
                 Object component = woko.getIoc().getComponent(componentKey);
                 if (component==null) {
                     logger.warn("Component " + componentKey + " not found in IoC ! Will not inject to " +
-                            facet.getClass().getName() + "#" + m.getName());
+                            clazz.getName() + "#" + m.getName());
                 } else {
 
                     // check if method has an appropriate signature
@@ -32,21 +36,64 @@ public class WokoInjectHelper {
                     if (paramTypes.length!=1) {
                         logger.warn("Trying to use @WokoInject on a method that has an invalid signature. \n" +
                                 "The method must accept a single parameter. \n" +
-                                "Cannot inject to " + facet.getClass().getName() + "#" + m.getName());
+                                "Cannot inject to " + clazz.getName() + "#" + m.getName());
                     } else {
 
                         // component found, and method takes 1 arg, try to invoke
                         try {
                             m.invoke(facet, component);
                         } catch(Exception e) {
-                            logger.error("Exception caught while trying to inject component " + component +
-                                " for key " + componentKey + " into " + facet.getClass().getName() + "#" + m.getName());
-                            throw new RuntimeException(e);
+                            String msg = "Exception caught while trying to inject component " + component +
+                                " for key " + componentKey + " into " + clazz.getName() + "#" + m.getName();
+                            logger.error(msg);
+                            throw new RuntimeException(msg, e);
                         }
                     }
                 }
             }
         }
+
+        Collection<Field> fields = ReflectUtil.getFields(clazz);
+        for (Field field : fields) {
+            WokoInject wokoInject = field.getAnnotation(WokoInject.class);
+            if (wokoInject!=null) {
+                if (logger.isDebugEnabled()) {
+                    logger.debug("@WokoInject found on field " + clazz.getName() + "#" + field.getName());
+                }
+                // field has @inject, try to find the setter
+                PropertyDescriptor pd = ReflectUtil.getPropertyDescriptor(clazz, field.getName());
+                boolean setterFound = false;
+                if (pd != null) {
+                    Method setter = pd.getWriteMethod();
+                    if (setter!=null) {
+                        setterFound = true;
+                        String componentKey = wokoInject.value();
+                        Object component = woko.getIoc().getComponent(componentKey);
+                        if (component==null) {
+                            logger.warn("Component " + componentKey + " not found in IoC ! Will not inject to " +
+                                clazz.getName() + "#" + field.getName());
+                        } else {
+                            try {
+                                setter.invoke(facet, component);
+                            } catch (Exception e) {
+                                String msg = "Exception caught while trying to inject component " + component +
+                                    " for key " + componentKey + " into field " + clazz.getName() + "#" + field.getName() +
+                                    " using setter.";
+                                logger.error(msg);
+                                throw new RuntimeException(msg, e);
+                            }
+                        }
+                    }
+                }
+                if (!setterFound) {
+                    String msg = "Field " + clazz.getName() + "#" + field.getName() + " + has @WokoInject but no property descriptor found (No public setter).";
+                    logger.error(msg);
+                    throw new RuntimeException(msg);
+                }
+            }
+        }
+
+
     }
 
 }
